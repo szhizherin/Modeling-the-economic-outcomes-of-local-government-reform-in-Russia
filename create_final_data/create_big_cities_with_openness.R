99ece5e1320a15c395f2bba309af3d5d3de7a1df
szhizherin <samzhizherin@gmail.com>
2022-06-27 12:49:49 +0300

    Create openness.xlsx

---
+++ create_final_data/create_big_cities_with_openness.R
@@ -0,0 +1,9 @@
+# Purpose: create big_cities_with_openness table for further analysis
+# Inputs:  final_data/big_cities.csv
+#          information_openness/raw_opennes.csv
+#          information_openness/matches.xlsx
+# Outputs: final_data/big_cities_with_openness.csv
+
+
+
+
